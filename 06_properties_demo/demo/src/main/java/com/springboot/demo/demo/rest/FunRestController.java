52437721feb729299d9fbc4c162a8bf70aa5057d
samuel.alonso <samuel.alonso@hacce.com>
2023-05-22 13:02:42 +0200

    spring propperties

---
+++ 06_properties_demo/demo/src/main/java/com/springboot/demo/demo/rest/FunRestController.java
@@ -0,0 +1,40 @@
+package com.springboot.demo.demo.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    /*expose "/" that returns a "Hello World!"*/
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello!";
+    }
+
+    /*expose a new endpoint for 'workout'*/
+    @GetMapping("/workout")
+    public String getDailyWorkOut(){
+        return "Run a hard 5k!";
+    }
+
+
+    /*expose a new endpoint for 'fortune'*/
+    @GetMapping("/fortune")
+    public String getFortune(){
+        return "Fortune!";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach name: " + coachName + " Team name: "  + teamName;
+    }
+
+}
