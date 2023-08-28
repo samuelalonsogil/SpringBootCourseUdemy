1b29e0f7dc0347c8c87f551b544cf60537c9a001
samuelalonsogil <asbarxas.samuelalonso@gmail.com>
2023-08-29 00:55:57 +0200

    new chapter

---
+++ 02-spring-boot-spring-core/02_component_scanning/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,17 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+/*makes the class available for dependency injection (makes it a Spring Bean)*/
+@Component
+public class CricketCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes!!!";
+    }
+
+    @Override
+    public String getDailyCoaching() { return "Good job !!";
+    }
+}
---
+++ 02-spring-boot-spring-core/02_component_scanning/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,42 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import com.luv2code.springcoredemo.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    /*define a private field for the dependency*/
+    private Coach myCoach;
+    private Player myPlayer;
+
+    /*define a constructor for dependency injection*/
+    @Autowired
+    public DemoController (Coach coach, Player player){
+        myCoach = coach;
+        myPlayer = player;
+
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello!";
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/dailycoaching")
+    public String getDailyCoaching(){
+        return myCoach.getDailyCoaching();
+    }
+
+    @GetMapping("/train")
+    public String train(){
+        return myPlayer.train();
+    }
+}
