13381f56643c481d22e4dac58083f8ab1485bc8d
samuel.alonso <samuel.alonso@hacce.com>
2023-10-19 00:16:10 +0200

    security

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,18 +2,57 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+/*import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;*/
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+
+
+    /*add support for JDBC no more hardcore users*/
     @Bean
+    public UserDetailsManager userDetailsManager (DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(
+                configurer -> configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+        /*use HTTP Basic Authentication*/
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        /*disable Cross Site Request Forgery (CSRF)*/
+        /* in general, not required for stateless REST APIs that use POST, PIT,DELETE and or PATH */
+        httpSecurity.csrf(csrf-> csrf.disable());
+
+        return httpSecurity.build();
+    }
+
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder().username("john").password("1234").roles("EMPLOYEE").build();
-        UserDetails mary = User.builder().username("mary").password("1234").roles("EMPLOYEE", "MANAGER").build();
-        UserDetails susan = User.builder().username("susan").password("1234").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+        //now SpringBoot will not use the user/pass from the application.properties file
+
+    UserDetails john = User.builder().username("john").password("{noop}1234").roles("EMPLOYEE").build();
+    UserDetails mary = User.builder().username("mary").password("{noop}1234").roles("EMPLOYEE", "MANAGER").build();
+    UserDetails susan = User.builder().username("susan").password("{noop}1234").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
-    }
+}
+*/
 }
